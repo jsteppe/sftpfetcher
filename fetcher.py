b99ab9a69d123c46a5557adcf7ea759f43f1976d
Yusif Galabayli <galabayli@gmail.com>
2019-03-07 14:14:59 +0400

    Renamed variable and function

--- fetcher.py
+++ fetcher.py
@@ -1,21 +1,21 @@
 import pysftp
 import os
 
-def qs():
+def prompts():
     """Asking for the remote machine and remote file parameters"""
     try:
         myHostname = input("Server name/IP: ")
         myUsername = input("Username: ")
         myPassword = input("Password for " + myHostname + ": ")
-        filename = input("Full path of the file you want to transfer from the remote machine: ")
+        remote_file = input("Full path of the file you want to transfer from the remote machine: ")
         home_dir = input("Local directory: ")
-        file_basename = os.path.basename(filename)
+        file_basename = os.path.basename(remote_file)
 
         with pysftp.Connection(host=myHostname, username=myUsername, password=myPassword) as sftp:
 
             print("Connection succesfully established. File transferred.")
     
-            remoteFilePath = filename
+            remoteFilePath = remote_file
             localFilePath = home_dir + file_basename
 
             sftp.get(remoteFilePath, localFilePath)
@@ -23,12 +23,13 @@ def qs():
             while True:
                 cont = input("Continue operations? [y/n]: ")
                 if cont == "y":
-                    qs()
+                    prompts()
                 elif cont == "n":
                     quit()
                 else:
                     continue
     except KeyboardInterrupt:
         print("Exiting...")
-qs()
+
+prompts()
 
